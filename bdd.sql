40ecc5283bfde51ca798e6007c59956b8eddf8cf
laurentPause <48903538+laurentPause@users.noreply.github.com>
2020-12-21 11:20:46 +0400

    mcd & script sql v1

--- bdd.sql
+++ bdd.sql
@@ -1,45 +1,3 @@
--- --------------------------------------------------------
--- Hôte :                        localhost
--- Version du serveur:           5.7.24 - MySQL Community Server (GPL)
--- SE du serveur:                Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Listage de la structure de la table anotes. asso_etablissements_users
-CREATE TABLE IF NOT EXISTS `asso_etablissements_users` (
-  `user_id` int(11) NOT NULL,
-  `etablissement_id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_at` datetime NOT NULL,
-  PRIMARY KEY (`user_id`,`etablissement_id`),
-  KEY `asso_etablissements_users_etablissement_id_foreign` (`etablissement_id`),
-  CONSTRAINT `asso_etablissements_users_etablissement_id_foreign` FOREIGN KEY (`etablissement_id`) REFERENCES `etablissements` (`etablissement_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `asso_etablissements_users_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
-
--- Listage de la structure de la table anotes. asso_sections_students
-CREATE TABLE IF NOT EXISTS `asso_sections_students` (
-  `student_id` int(11) NOT NULL,
-  `section_id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_at` datetime NOT NULL,
-  PRIMARY KEY (`student_id`,`section_id`),
-  KEY `asso_sections_students_section_id_foreign` (`section_id`),
-  CONSTRAINT `asso_sections_students_section_id_foreign` FOREIGN KEY (`section_id`) REFERENCES `sections` (`section_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `asso_sections_students_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. asso_tags_students
 CREATE TABLE IF NOT EXISTS `asso_tags_students` (
@@ -53,8 +11,6 @@ CREATE TABLE IF NOT EXISTS `asso_tags_students` (
   CONSTRAINT `asso_tags_students_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `asso_tags_students_tag_id_foreign` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`tag_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. chapitres
 CREATE TABLE IF NOT EXISTS `chapitres` (
@@ -71,39 +27,6 @@ CREATE TABLE IF NOT EXISTS `chapitres` (
   CONSTRAINT `chapitres_grille_id_foreign` FOREIGN KEY (`grille_id`) REFERENCES `grilles` (`grille_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- Les données exportées n'étaient pas sélectionnées.
-
--- Listage de la structure de la table anotes. comments
-CREATE TABLE IF NOT EXISTS `comments` (
-  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
-  `content` text,
-  `student_id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_at` datetime NOT NULL,
-  PRIMARY KEY (`comment_id`),
-  KEY `comments_student_id_foreign` (`student_id`),
-  CONSTRAINT `comments_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
-
--- Listage de la structure de la table anotes. contacts
-CREATE TABLE IF NOT EXISTS `contacts` (
-  `contact_id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) NOT NULL,
-  `prenom` varchar(255) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `telephone` varchar(255) DEFAULT NULL,
-  `autre` text,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_at` datetime NOT NULL,
-  PRIMARY KEY (`contact_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
-
 -- Listage de la structure de la table anotes. criteres
 CREATE TABLE IF NOT EXISTS `criteres` (
   `critere_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -119,7 +42,6 @@ CREATE TABLE IF NOT EXISTS `criteres` (
   CONSTRAINT `criteres_chapitre_id_foreign` FOREIGN KEY (`chapitre_id`) REFERENCES `chapitres` (`chapitre_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. etablissements
 CREATE TABLE IF NOT EXISTS `etablissements` (
@@ -132,78 +54,35 @@ CREATE TABLE IF NOT EXISTS `etablissements` (
   PRIMARY KEY (`etablissement_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. evaluations
 CREATE TABLE IF NOT EXISTS `evaluations` (
   `evaluation_id` int(11) NOT NULL AUTO_INCREMENT,
   `date_evaluation` datetime NOT NULL,
   `comment` text,
+  `notes` text,
   `grille_id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
-  `section_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_at` datetime NOT NULL,
   PRIMARY KEY (`evaluation_id`),
   KEY `evaluations_grille_id_foreign` (`grille_id`),
   KEY `evaluations_student_id_foreign` (`student_id`),
-  KEY `evaluations_section_id_foreign` (`section_id`),
   CONSTRAINT `evaluations_grille_id_foreign` FOREIGN KEY (`grille_id`) REFERENCES `grilles` (`grille_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `evaluations_section_id_foreign` FOREIGN KEY (`section_id`) REFERENCES `sections` (`section_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `evaluations_student_id_foreign` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. grilles
 CREATE TABLE IF NOT EXISTS `grilles` (
   `grille_id` int(11) NOT NULL AUTO_INCREMENT,
   `libelle` varchar(255) NOT NULL,
   `description` text,
-  `etablissement_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_at` datetime NOT NULL,
   PRIMARY KEY (`grille_id`),
-  KEY `grilles_etablissement_id_foreign` (`etablissement_id`),
-  CONSTRAINT `grilles_etablissement_id_foreign` FOREIGN KEY (`etablissement_id`) REFERENCES `etablissements` (`etablissement_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
-
--- Listage de la structure de la table anotes. migrations
-CREATE TABLE IF NOT EXISTS `migrations` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `version` varchar(255) NOT NULL,
-  `class` text NOT NULL,
-  `group` varchar(255) NOT NULL,
-  `namespace` varchar(255) NOT NULL,
-  `time` int(11) NOT NULL,
-  `batch` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=290 DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
-
--- Listage de la structure de la table anotes. notes
-CREATE TABLE IF NOT EXISTS `notes` (
-  `note_id` int(11) NOT NULL AUTO_INCREMENT,
-  `value` varchar(255) NOT NULL,
-  `comment` text,
-  `evaluation_id` int(11) NOT NULL,
-  `critere_id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_at` datetime NOT NULL,
-  PRIMARY KEY (`note_id`),
-  KEY `notes_evaluation_id_foreign` (`evaluation_id`),
-  KEY `notes_critere_id_foreign` (`critere_id`),
-  CONSTRAINT `notes_critere_id_foreign` FOREIGN KEY (`critere_id`) REFERENCES `criteres` (`critere_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `notes_evaluation_id_foreign` FOREIGN KEY (`evaluation_id`) REFERENCES `evaluations` (`evaluation_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. roles
 CREATE TABLE IF NOT EXISTS `roles` (
@@ -219,7 +98,6 @@ CREATE TABLE IF NOT EXISTS `roles` (
   UNIQUE KEY `role_libelle` (`role_libelle`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. sections
 CREATE TABLE IF NOT EXISTS `sections` (
@@ -228,15 +106,16 @@ CREATE TABLE IF NOT EXISTS `sections` (
   `description` text,
   `annee` varchar(4) NOT NULL,
   `etablissement_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_at` datetime NOT NULL,
   PRIMARY KEY (`section_id`),
   KEY `sections_etablissement_id_foreign` (`etablissement_id`),
+  KEY `sections_users_id_foreign` (`user_id`),
   CONSTRAINT `sections_etablissement_id_foreign` FOREIGN KEY (`etablissement_id`) REFERENCES `etablissements` (`etablissement_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `sections_users_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. students
 CREATE TABLE IF NOT EXISTS `students` (
@@ -246,16 +125,16 @@ CREATE TABLE IF NOT EXISTS `students` (
   `email` varchar(255) DEFAULT NULL,
   `telephone` varchar(255) DEFAULT NULL,
   `autre` text,
-  `etablissement_id` int(11) NOT NULL,
+  `comment` text,
+  `section_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_at` datetime NOT NULL,
   PRIMARY KEY (`student_id`),
-  KEY `students_etablissement_id_foreign` (`etablissement_id`),
-  CONSTRAINT `students_etablissement_id_foreign` FOREIGN KEY (`etablissement_id`) REFERENCES `etablissements` (`etablissement_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `students_sections_id_foreign` (`section_id`),
+  CONSTRAINT `students_sections_id_foreign` FOREIGN KEY (`section_id`) REFERENCES `sections` (`section_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. tags
 CREATE TABLE IF NOT EXISTS `tags` (
@@ -264,17 +143,12 @@ CREATE TABLE IF NOT EXISTS `tags` (
   `libelle` varchar(255) NOT NULL,
   `color` varchar(20) NOT NULL,
   `description` text,
-  `etablissement_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_at` datetime NOT NULL,
   `color_text` varchar(20) NOT NULL DEFAULT '#000000',
   PRIMARY KEY (`tag_id`),
-  KEY `tags_etablissement_id_foreign` (`etablissement_id`),
-  CONSTRAINT `tags_etablissement_id_foreign` FOREIGN KEY (`etablissement_id`) REFERENCES `etablissements` (`etablissement_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Les données exportées n'étaient pas sélectionnées.
 
 -- Listage de la structure de la table anotes. users
 CREATE TABLE IF NOT EXISTS `users` (
